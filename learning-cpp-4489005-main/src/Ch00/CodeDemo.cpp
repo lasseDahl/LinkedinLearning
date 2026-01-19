d20593b6df2939493d9284c862eaee9d9baae495
Lasse Dahl <76868023+lasseDahl@users.noreply.github.com>
2026-01-19 10:29:38 +0000

    commit test mod

--- learning-cpp-4489005-main/src/Ch00/CodeDemo.cpp
+++ learning-cpp-4489005-main/src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 commit test mod
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -10,7 +10,7 @@ int main(){
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
-    std::cin >> num_2;
+4    std::cin >> num_2;
     
     result = num_1 + num_2;
     
