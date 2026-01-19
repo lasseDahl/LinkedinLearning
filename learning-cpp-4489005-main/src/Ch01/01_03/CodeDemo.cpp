5f2e3542fc78ea37b23c8aca7aeaacf8528383b8
Dahl, Lasse <lasse.dahl@barco.com>
2026-01-19 11:09:47 +0100

    added lession

---
+++ learning-cpp-4489005-main/src/Ch01/01_03/CodeDemo.cpp
@@ -0,0 +1,15 @@
+// Learning C++ 
+// Challenge 01_03
+// Console Interaction, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cin >> str;
+    std::cout << str;
+
+    std::cout << std::endl << std::endl;
+    return (0);
+}
